f6278e40ce9609bf80f1937d1cce433181fd35a1
aarongmez18 <aarongmez18@gmail.com>
2025-07-15 18:56:58 +0200

    2º Conceptos Basicos: Variables y tipo de datos sencillos

---
+++ Conceptos Basicos/datos-simples.py
@@ -0,0 +1,23 @@
+# Tipo texto
+"String con dos comillas"
+'String con una comilla'
+
+"""String
+En diferentes
+Lineas con 3 comillas"""
+
+'''String
+En Diferentes
+Lineas con 3 comillas simples'''
+
+# Tipo Entero
+print(4)
+
+# Tipo Decimales
+print(40.2)
+
+# Boolean LA PRIMERA EN MAYÚSCULAS
+False
+True
+
+
---
+++ Conceptos Basicos/variables.py
@@ -0,0 +1,28 @@
+a = 2
+b = 3
+c = a + 2
+print(c)
+c += 1
+print (c)
+
+nombre = "Aarón "
+print(nombre)
+
+# Concatenar
+apellido = "Gómez"
+print(nombre + apellido)
+
+# Concatenar con f-strings
+bienvenida = f"Hola {nombre + apellido} ¿Como estas?"
+print(bienvenida)
+
+# Elimina datos que se almacena en la memoria
+# del bienvenida
+
+
+# OPERADORES DE PERTENENCIA (in y in not)
+# El in busca en el texto lo que contiene la variable y devuelve boolean
+print("Salida variable bienvenida con in Ejemplo: Hola" in bienvenida)
+
+# El not in busca en el texto lo que contiene la variable y devuelve boolean
+print("Salida variable bienvenida con in Ejemplo: Hola" not in bienvenida)
