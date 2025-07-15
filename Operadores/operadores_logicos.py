059f40eb66267637030ad007941fee6c7e44c42d
aarongmez18 <aarongmez18@gmail.com>
2025-07-15 19:29:37 +0200

    5.3º Operadores Logicos

---
+++ Operadores/operadores_logicos.py
@@ -0,0 +1,33 @@
+# AND
+resultado = True & True    # Devuelve True
+resultado2 = False & True  # Devuelve False
+resultado3 = True & False  # Devuelve False
+resultado4 = False & False # Devuelve False
+
+print("AND")
+print(resultado)
+print(resultado2)
+print(resultado3)
+print(resultado4)
+
+# OR
+resultado = True | True    # Devuelve True
+resultado2 = False | True  # Devuelve True
+resultado3 = True | False  # Devuelve True
+resultado4 = False | False # Devuelve False
+
+print("OR")
+print(resultado)
+print(resultado2)
+print(resultado3)
+print(resultado4)
+
+# NOT
+resultado = not True       # Devuelve False
+resultado2 = not False     # Devuelve True
+
+print("NOT")
+print(resultado)
+print(resultado2)
+print(resultado3)
+print(resultado4)
