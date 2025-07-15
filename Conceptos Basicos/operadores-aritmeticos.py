1a4f2ba96c7c335394d59b6326e30e282c50e24f
aarongmez18 <aarongmez18@gmail.com>
2025-07-15 19:13:08 +0200

    4ª Operadores Aritmeticos

---
+++ Conceptos Basicos/operadores-aritmeticos.py
@@ -0,0 +1,22 @@
+# Suma
+print(1 + 1)
+
+# Resta
+print(1 - 3)
+
+# Multiplicación o producto
+print(1 * 3)
+
+# División
+print(1 / 3)
+
+# Resto (Resto de la división)
+print(1 % 3)
+
+# Exponente
+print(1**3)
+
+# División (Devuelve el entero de una división)
+print(1//3)
+
+tipo_de_dato = type(5) # Nos devuelve que tipo de dato es
